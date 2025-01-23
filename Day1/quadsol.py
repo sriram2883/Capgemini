29675e1af1a3d2057f8d7e5280335dae8cb206b9
Sriram Elluru <ellurisriram27655@gmail.com>
2025-01-23 16:21:46 +0530

    Day1

---
+++ Day1/quadsol.py
@@ -0,0 +1,7 @@
+a=int(input("Enter the value of a: "))
+b=int(input("Enter the value of b: "))
+c=int(input("Enter the value of c: "))
+d=(b**2)-(4*a*c)
+sol1=(-b+math.sqrt(d))/(2*a)
+sol2=(-b-math.sqrt(d))/(2*a)
+print("The solutions are",sol1,"and",sol2)

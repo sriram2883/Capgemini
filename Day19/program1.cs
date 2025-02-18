4f4265a44839d6b4f8d51b451778b1caa5b0b463
Sriram Elluru <ellurisriram27655@gmail.com>
2025-02-18 14:38:21 +0530

    Day19

---
+++ Day19/program1.cs
@@ -0,0 +1,12 @@
+using namespace program1
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = int.MaxValue;
+            console.WriteLine("a = " + ( a + b ));
+        }
+    }
+}
